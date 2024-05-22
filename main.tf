09c0fdd672e816d84151e0ac74338f55f329fb67
sankalpmh <sankalpmt@gmail.com>
2024-05-22 18:22:19 +0530

    apigw rule changes

--- main.tf
+++ main.tf
@@ -49,8 +49,7 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  }
-
+  
     security_rule {
     name                       = "apigw"
     priority                   = 1003
