5ce0456d56bf92f6b5e682782c06b5257c8a4307
sankalpmh <sankalpmt@gmail.com>
2024-05-22 18:21:16 +0530

    Inbound Rule Changes

--- main.tf
+++ main.tf
@@ -49,6 +49,19 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  }
+
+    security_rule {
+    name                       = "apigw"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create a network interface
